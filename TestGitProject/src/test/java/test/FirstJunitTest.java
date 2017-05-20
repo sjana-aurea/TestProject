b172f487d5efaa948c5dc0880e1e10199cde41de
sjana-aurea <sitam.jana@aurea.com>
2017-05-20 20:04:00 +0530

    Added first git project

---
+++ TestGitProject/src/test/java/test/FirstJunitTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import library.FunctionLibrary;
+
+public class FirstJunitTest {
+	
+	private static FunctionLibrary functionLibrary;
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		functionLibrary = new FunctionLibrary(driver);
+		driver = functionLibrary.openBrowser();
+	}
+	
+	@Test
+	public void test() {
+		functionLibrary.navigate("http://www.seleniumhq.org/");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("@AfterClass");
+	}
+
+}
